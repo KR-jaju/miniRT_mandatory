509cf6730af4cf3849060765f0e970e9f4a10c63
yonaMac <yonaimyo@gmail.com>
2023-08-29 05:15:25 +0900

    Feat: libmlx 라이브러리 추가

---
+++ lib/libmlx/mlx_opengl.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mlx_opengl.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yeonhkim  <yeonhkim@student.42seoul.>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/27 13:40:42 by yeonhkim          #+#    #+#             */
+/*   Updated: 2023/02/27 13:40:42 by yeonhkim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+**
+** mlx_opengl.h 
+**
+** public include, use it after mlx.h
+** designed only for minilibx_macos
+**
+*/
+
+void    *mlx_new_opengl_window(void *mlx_ptr, int size_x, int size_y, char *title);
+
+/* create an opengl window. put_image & pixel_put & string_put do not work there. */
+
+int	mlx_opengl_swap_buffers(void *win_ptr);
+
+/* the created window is double buffered. Use this funct to swap buffers */
+/* this funct will call glFlush(). Don't call it. */
+
+int	mlx_opengl_window_set_context(void *win_ptr);
+
+/* in case multiple opengl windows are present, change opengl active context */
