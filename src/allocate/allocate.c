8c818837ac1ad4f31cbdf45b1f3d34c579952051
KR-jaju <wnwoals0127@gmail.com>
2023-08-31 19:34:28 +0900

    Feat: allocate함수 위치 변경, t_byte -> uint8_t

---
+++ src/allocate/allocate.c
@@ -0,0 +1,20 @@
+
+#include <allocate.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+void	*allocate(size_t size)
+{
+	uint8_t*const	ptr = malloc(size);
+	size_t			i;
+
+	if (ptr == (void *)0)
+		exit(1);
+	i = 0;
+	while (i < size)
+	{
+		ptr[i] = (uint8_t)0;
+		i++;
+	}
+	return (ptr);
+}
