b873283b16965b30518e31e9e8fc747669c79b5e
yonaMac <yonaimyo@gmail.com>
2023-09-01 23:52:24 +0900

    Fix: libft.h 빠뜨린 ft_strcmp 추가

--- lib/libft/libft.h
+++ lib/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: yona <yona@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/27 13:40:42 by yeonhkim          #+#    #+#             */
-/*   Updated: 2023/08/31 23:52:48 by yona             ###   ########.fr       */
+/*   Updated: 2023/09/01 23:50:29 by yona             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,6 +43,7 @@ char			*ft_strchr(const char *s, int c);
 char			*ft_strrchr(const char *s, int c);
 char			*ft_strnstr(const char *haystack, const char *needle, \
 							size_t len);
+int				ft_strcmp(const char *s1, const char *s2);
 int				ft_strncmp(const char *s1, const char *s2, size_t n);
 size_t			ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t			ft_strlcat(char *dst, const char *src, size_t dstsize);
