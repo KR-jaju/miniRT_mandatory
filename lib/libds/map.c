0548bb673d8fb1a0e53486dc9e3e886ddc9ef56f
yonaCluster <yonaimyo@gmail.com>
2023-09-01 18:41:15 +0900

    Comment: map.c 컴파일 오류나는 코드 주석처리

--- lib/libds/map.c
+++ lib/libds/map.c
@@ -1,24 +1,20 @@
-
-#include <stdlib.h>
-#include <stdint.h>
-#include <stddef.h>
-#include "allocate.h"
 #include "libds.h"
-#include "libft.h"
 
-static
-uint32_t	index_of_key(t_map *this, const char *key)
-{
-	char	*tmp;
+// TODO: 컴파일 오류 발생하여 수정 필요
 
-	while (i < this->key->count)
-	{
-		if (ft_strcmp((list_get(&this->key, i, &tmp), (char *)tmp), key) == 0)
-			return (i);
-		i++;
-	}
-	return (-1);
-}
+// static
+// uint32_t	index_of_key(t_map *this, const char *key)
+// {
+// 	char	*tmp;
+
+// 	while (i < this->key->count)
+// 	{
+// 		if (ft_strcmp((list_get(&this->key, i, &tmp), (char *)tmp), key) == 0)
+// 			return (i);
+// 		i++;
+// 	}
+// 	return (-1);
+// }
 
 void	map_init(t_map *this, size_t type_size, size_t capacity)
 {
@@ -26,22 +22,22 @@ void	map_init(t_map *this, size_t type_size, size_t capacity)
 	list_init(&this->value, type_size, capacity);
 }
 
-void	map_put(t_map *this, const char *key, void *data)
-{
-	uint32_t	entry_idx;
+// void	map_put(t_map *this, const char *key, void *data)
+// {
+// 	uint32_t	entry_idx;
 
-	entry_idx = index_of_key(this, key);
-	if (entry_idx != -1)
-		return (list_set(this->value, entry_idx, data));
-	list_push(this->key, &ft_strdup(key));
-	list_push(this->value, data);
-}
+// 	entry_idx = index_of_key(this, key);
+// 	if (entry_idx != -1)
+// 		return (list_set(this->value, entry_idx, data));
+// 	list_push(this->key, &ft_strdup(key));
+// 	list_push(this->value, data);
+// }
 
-void	map_get(t_map *this, const char *key, void *out)
-{
-	uint32_t	entry_idx;
+// void	map_get(t_map *this, const char *key, void *out)
+// {
+// 	uint32_t	entry_idx;
 
-	entry_idx = index_of_key(this, key);
-	if (entry_idx != -1)
-		return (list_get(this->value, entry_idx, out));
-}
+// 	entry_idx = index_of_key(this, key);
+// 	if (entry_idx != -1)
+// 		return (list_get(this->value, entry_idx, out));
+// }
