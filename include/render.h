b03e68f63ca217fb855bbc1ac51b89404cc1a838
yonaMac <yonaimyo@gmail.com>
2023-09-05 11:38:33 +0900

    Feat: s_hit_record 구조체 추가

--- include/render.h
+++ include/render.h
@@ -10,6 +10,11 @@ typedef struct s_ray
 	t_vec3	dir;
 }t_ray;
 
+typedef struct s_hit_record
+{
+	t_object	*object;
+	float		t; // 카메라 레이 디렉션 스칼라
+}t_hit_record;
 
 t_vec3	calculate_pixel_color(int x, int y, t_scene *scene);
 
