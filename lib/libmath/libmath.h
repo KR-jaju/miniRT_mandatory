984a1f218bcabc4a747b50424d441835c1f2ae8c
yonaMac <yonaimyo@gmail.com>
2023-09-05 23:22:28 +0900

    Feat: libmath 업데이트
    - 말도 안되는 함수 mat4_mul_vec3 삭제
    - normalize 함수 추가
    - 곱셈 함수 이름 변경 (mulmm, mulmv)
    - 역행렬 함수 원형 추가

--- lib/libmath/libmath.h
+++ lib/libmath/libmath.h
@@ -40,25 +40,28 @@ typedef struct s_mat4 {
 t_vec3		vec3(float x, float y, float z);
 t_vec3		vec3_add(t_vec3 v1, t_vec3 v2);
 t_vec3		vec3_sub(t_vec3 v1, t_vec3 v2);
-t_vec3		vec3_mul(t_vec3 v1, float a);
+t_vec3		vec3_mul(t_vec3 v, float a);
 float		vec3_dot(t_vec3 v1, t_vec3 v2);
 t_vec3		vec3_cross(t_vec3 v1, t_vec3 v2);
 float		vec3_length(t_vec3 v);
+t_vec3		vec3_normalize(t_vec3 v);
 
 // vector4
 t_vec4		vec4(float x, float y, float z, float w);
 t_vec4		vec4_add(t_vec4 v1, t_vec4 v2);
 t_vec4		vec4_sub(t_vec4 v1, t_vec4 v2);
+t_vec4		vec4_mul(t_vec4 v, float a);
 float		vec4_dot(t_vec4 v1, t_vec4 v2);
 float		vec4_length(t_vec4 v);
+t_vec4		vec4_normalize(t_vec4 v);
 
 // matrix
 t_mat4		unit_mat4(void);
-t_mat4		mat4_mul(t_mat4 m1, t_mat4 m2);
+t_mat4		mat4_mulmm(t_mat4 m1, t_mat4 m2);
+t_mat4		mat4_inverse(t_mat4 m);
 
 // transform
-t_vec3		mat4_mul_vec3(t_mat4 m, t_vec3 v);
-t_vec4		mat4_mul_vec4(t_mat4 m, t_vec4 v);
+t_vec4		mat4_mulmv(t_mat4 m, t_vec4 v);
 void		compose_scale_mat4(t_mat4 *m, float x, float y, float z);
 void		compose_rotate_mat4(t_mat4 *m, float x, float y, float z);
 void		compose_move_mat4(t_mat4 *m, float x, float y, float z);
