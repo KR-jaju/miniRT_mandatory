932ada6465055c307ac8a14e71456a23a0b576e8
yonaMac <yonaimyo@gmail.com>
2023-09-05 11:38:16 +0900

    Feat: s_ray 구조체 추가

--- include/render.h
+++ include/render.h
@@ -4,6 +4,13 @@
 # include "libmath.h"
 # include "scene.h"
 
+typedef struct s_ray
+{
+	t_vec3	origin;
+	t_vec3	dir;
+}t_ray;
+
+
 t_vec3	calculate_pixel_color(int x, int y, t_scene *scene);
 
 #endif
