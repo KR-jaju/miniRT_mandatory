c3f3a23ed5d10b08c14e601cd8f85acac17ca2f6
yonaMac <yonaimyo@gmail.com>
2023-09-12 13:05:30 +0900

    Feat: 쉐이딩을 위한 direction 벡터 구하는 함수들 추가
    - incident, reflection, view(입사, 반사, 시선)

--- include/render.h
+++ include/render.h
@@ -35,4 +35,12 @@ t_vec3	shade_intersection(t_hit_record *hit, t_scene *scene);
 t_vec3	camera_ray_direction(int x, int y, t_camera *cam, t_image *img);
 bool	ray_object_intersection(t_ray *ray, t_object *object, \
 								t_hit_record *record);
+
+// direction
+t_vec3	incident_direction(t_vec3 hit_pos, t_vec3 light_pos);
+t_vec3	reflection_direction(t_vec3 i, t_vec3 n);
+t_vec3	view_direction(t_vec3 camera_pos, t_vec3 hit_pos);
+
+
+
 #endif 
