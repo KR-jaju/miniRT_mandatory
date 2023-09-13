5ea126e282eae6cbbf9e119a8a36e955cddef23a
yonaMac <yonaimyo@gmail.com>
2023-09-13 21:29:46 +0900

    Refactor: norm 준수

--- src/render/matrix_TRS.c
+++ src/render/matrix_TRS.c
@@ -3,13 +3,13 @@
 
 t_mat4	translate_matrix(t_vec3 t)
 {
-	const float values[16] = {
+	const float	values[16] = {
 		1, 0, 0, 0,
 		0, 1, 0, 0,
 		0, 0, 1, 0,
 		t.x, t.y, t.z, 1
 	};
-	t_mat4				m;
+	t_mat4		m;
 
 	ft_memcpy(&m, values, sizeof(m));
 	return (m);
@@ -45,13 +45,13 @@ t_mat4	rotate_matrix(t_vec3 r)
 
 t_mat4	scale_matrix(t_vec3 s)
 {
-	const float values[16] = {
+	const float	values[16] = {
 		s.x, 0, 0, 0,
 		0, s.y, 0, 0,
 		0, 0, s.z, 0,
 		0, 0, 0, 1
 	};
-	t_mat4				m;
+	t_mat4		m;
 
 	ft_memcpy(&m, values, sizeof(m));
 	return (m);
