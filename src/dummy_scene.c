9664054ff1f830fad618c9ff5f0992ca4fd05e7e
yonaCluster <yonaimyo@gmail.com>
2023-09-15 15:48:03 +0900

    Feat: 더미 씬 업데이트

--- src/dummy_scene.c
+++ src/dummy_scene.c
@@ -1,10 +1,67 @@
 #include <stdlib.h>
 #include "scene.h"
 
+t_mesh	mesh_plane(void)
+{
+	t_mesh plane;
+
+	plane.vertices = malloc(sizeof(t_vec3) * 4);
+	plane.normals = malloc(sizeof(t_vec3) * 4);
+	plane.indices = malloc(sizeof(int) * 6);
+
+	plane.vertices[0] = (t_vec3){-5, 0, 5};
+	plane.vertices[1] = (t_vec3){-5, 0, -5};
+	plane.vertices[2] = (t_vec3){5, 0, -5};
+	plane.vertices[3] = (t_vec3){5, 0, 5};
+
+	plane.normals[0] = (t_vec3){0, 0, 1};
+	plane.normals[1] = (t_vec3){0, 0, 1};
+	plane.normals[2] = (t_vec3){0, 0, 1};
+	plane.normals[3] = (t_vec3){0, 0, 1};
+
+// polygon 1
+	plane.indices[0] = 0;
+	plane.indices[1] = 1;
+	plane.indices[2] = 2;
+// polygon 2
+	plane.indices[3] = 0;
+	plane.indices[4] = 2;
+	plane.indices[5] = 3;
+
+	plane.n_vertices = 4;
+	plane.n_indices = 6;
+	plane.n_polygons = 2; // 생각해보니 무조건 indices / 3 아닌가?
+
+	return (plane);
+}
+
+t_object	dummy_object(t_mesh *mesh, t_vec3 color)
+{
+	t_object object;
+
+	object.mesh = mesh;
+	object.position = (t_vec3){0, 0, 0};
+	object.rotation = (t_vec3){0, 0, 0};
+	object.scale = (t_vec3){1, 1, 1};
+	object.color = color;
+
+	object.vertices = malloc(sizeof(t_vec3) * mesh->n_vertices);
+	object.normals = malloc(sizeof(t_vec3) * mesh->n_vertices);
+
+	return (object);
+}
+
 int	dummy_scene(t_scene *scene)
 {
-	scene->objects = NULL;
-	scene->meshs = NULL;
+	const int n_meshs = 1;
+	const int n_objects = 1;
+	scene->n_meshs = n_meshs;
+	scene->n_objects = n_objects;
+	scene->meshs = malloc(sizeof(t_object) * n_meshs);
+	scene->objects = malloc(sizeof(t_object) * n_objects);
+
+	scene->meshs[0] = mesh_plane();
+	scene->objects[0] = dummy_object(&scene->meshs[0], (t_vec3){1, 0, 0});
 
 	scene->camera.eye = (t_vec3){0, 5, 0};
 	scene->camera.look_at = (t_vec3){5, 0, 0};
@@ -14,6 +71,6 @@ int	dummy_scene(t_scene *scene)
 	scene->light.position = (t_vec3){0, 0, 0};
 	scene->light.color = (t_vec3){1, 0, 0};
 
-	scene->ambient_light = (t_vec3){0, 0, 0.5};
+	scene->ambient_light = (t_vec3){0, 0.5, 0};
 	return (0);
 }
