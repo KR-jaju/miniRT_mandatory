c6a0a6afacf14c71b475edfc6a19ea5629201a9d
yonaCluster <yonaimyo@gmail.com>
2023-09-16 05:07:18 +0900

    Fix: default material 초기화 오류 수정

--- src/render/shading.c
+++ src/render/shading.c
@@ -1,14 +1,14 @@
 #include "render.h"
-#include "debug.h"
 
 static t_material	default_material(t_vec3 color)
 {
 	static t_material	material = {\
 									(t_vec3){0, 0, 0}, \
+									REFLECTIVITY, \
 									K_DIFFUSE, \
 									K_SPECULAR, \
 									SHININESS, \
-									REFLECTIVITY};
+									};
 
 	material.color = color;
 	return (material);
