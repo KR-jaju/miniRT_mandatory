518c918dae63de22f51f7d1e373ca25c9ce52540
yonaMac <yonaimyo@gmail.com>
2023-09-19 23:16:47 +0900

    Feat: shutdown시 콘솔 출력 추가

--- src/shutdown.c
+++ src/shutdown.c
@@ -1,8 +1,11 @@
 #include <stdlib.h>
+#include "miniRT.h"
+#include "console.h"
 
 int	shutdown_program(void *resource)
 {
 	// TODO: 할당된 메모리 해제
 	(void)resource;
+	console_msg(ANSI_BLUE""MSG_SHUTDOWN);
 	exit(0);
 }
