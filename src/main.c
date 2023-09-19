b252bbe2473b528f25db50a6af498210ce424fa9
yonaMac <yonaimyo@gmail.com>
2023-09-19 14:12:23 +0900

    Fix: 외적 벡터 방향 실수 고침

--- src/main.c
+++ src/main.c
@@ -62,8 +62,8 @@ static void	fill_triangle_info(int nth, t_object *object, t_triangle *tri)
 	tri->vertices[1] = vertices[idx[1]];
 	tri->vertices[2] = vertices[idx[2]];
 	tri->face_normal = vec3_normalize(\
-						vec3_cross(vec3_sub(tri->vertices[1], tri->vertices[0]), \
-								vec3_sub(tri->vertices[2], tri->vertices[0])));
+						vec3_cross(vec3_sub(tri->vertices[2], tri->vertices[0]), \
+								vec3_sub(tri->vertices[1], tri->vertices[0])));
 }
 
 static void	fill_triangles(t_scene *scene)
