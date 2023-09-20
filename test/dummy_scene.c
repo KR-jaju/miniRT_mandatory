f7bfac828ee9af5a99e8d2a9c3341d0b8b111326
yonaMac <yonaimyo@gmail.com>
2023-09-20 16:44:34 +0900

    Test: 더미 씬에 직접 구현한 플레인 추가

--- test/dummy_scene.c
+++ test/dummy_scene.c
@@ -3,40 +3,6 @@
 #include "mesh.h"
 #include "debug.h"
 
-t_mesh	mesh_plane(void)
-{
-	t_mesh	plane;
-
-	plane.vertices = malloc(sizeof(t_vec3) * 4);
-	plane.vertex_normals = malloc(sizeof(t_vec3) * 4);
-	plane.indices = malloc(sizeof(int) * 6);
-
-	plane.vertices[0] = (t_vec3){-5, 0, 5};
-	plane.vertices[1] = (t_vec3){-5, 0, -5};
-	plane.vertices[2] = (t_vec3){5, 0, -5};
-	plane.vertices[3] = (t_vec3){5, 0, 5};
-
-	plane.vertex_normals[0] = (t_vec3){0, 1, 0};
-	plane.vertex_normals[1] = (t_vec3){0, 1, 0};
-	plane.vertex_normals[2] = (t_vec3){0, 1, 0};
-	plane.vertex_normals[3] = (t_vec3){0, 1, 0};
-
-// polygon 1
-	plane.indices[0] = 0;
-	plane.indices[1] = 1;
-	plane.indices[2] = 2;
-// polygon 2
-	plane.indices[3] = 0;
-	plane.indices[4] = 2;
-	plane.indices[5] = 3;
-
-	plane.n_vertices = 4;
-	plane.n_indices = 6;
-	plane.n_triangles = 2; // 생각해보니 무조건 indices / 3 아닌가?
-
-	return (plane);
-}
-
 t_object	dummy_object(t_mesh *mesh, t_vec3 color)
 {
 	t_object object;
@@ -63,7 +29,7 @@ int	dummy_scene(t_scene *scene)
 	//scene->mesh = malloc(sizeof(t_object) * n_meshs);
 	scene->objects = malloc(sizeof(t_object) * n_objects);
 
-	scene->meshs[0] = mesh_plane();
+	plane_init(&scene->meshs[0]);
 	cylinder_init(&scene->meshs[1], 10);
 	scene->objects[0] = dummy_object(&scene->meshs[0], (t_vec3){0, 0, 1});
 
