9fbe638882659cdad1f4c87bfd3cb1d4b3f06a16
yonaMac <yonaimyo@gmail.com>
2023-09-21 03:01:18 +0900

    Feat: camera 전처리 함수 완성

--- src/preprocess/camera.c
+++ src/preprocess/camera.c
@@ -1,5 +1,28 @@
 // dummy
 // TODO: 카메라 구조체 관련 전처리 작업 함수 작성 (행렬 구하기 등)
+#include "scene.h"
+#include "settings.h"
+#include "render.h"
+
+static t_vec3	ndc_to_world_space(t_vec3 ndc, const t_mat4 *pv_inverse)
+{
+	t_vec4	v4;
+
+	v4 = mat4_mulmv(*pv_inverse, (t_vec4){ndc.x, ndc.y, ndc.z, 1});
+	vec4_mul(v4, (1 / v4.w));
+	return ((t_vec3){v4.x, v4.y, v4.z});
+}
+
+static t_vec3	screen_to_ndc(int screen_x, int screen_y, int width, int height)
+{
+	t_vec3	ndc;
+
+	ndc.x = ((screen_x + 0.5) / width) * 2 - 1;
+	ndc.y = 1 - ((screen_y + 0.5) / height) * 2;
+	ndc.z = 0; // -1~1 사이면 아무 값이나 상관없음
+	return (ndc);
+}
+
 /*
 world space상에서의 픽셀 좌표 구하기
 1. NDC 좌표로 변환 (=[-1,1] 범위로 매핑)
@@ -10,29 +33,24 @@ world space상에서의 픽셀 좌표 구하기
 구해야하는 이미지 플레인 모서리 좌표
 (0, 0), (WIDTH, 0), (0, HEIGHT), (WIDTH, HEIGHT)
 */
+// TODO: image 구조체를 매개변수로 받는게 맞을지 고민하고 결정하기
 void camera_fill_corners_world_pos(t_camera *cam, t_image *img)
 {
 	const t_mat4	pv_inverse = mat4_inverse(mat4_mulmm(\
 			projection_matrix(cam->fov, img->width / img->height, NEAR, FAR), \
 			view_matrix(cam->right, cam->up, cam->forward, cam->position)));
-	t_vec4			corners[4];
+	int				corners[4][2] = {{0, 0}, \
+									{img->width, 0}, \
+									{0, img->height}, \
+									{img->width, img->height}};
 	t_vec3			ndc;
 	int				i;
 
-	corners[0] = vec4(0, 0, 0, 1);
-	corners[1] = vec4(img->width, 0, 0, 1);
-	corners[2] = vec4(0, img->height, 0, 1);
-	corners[3] = vec4(img->width, img->height, 0, 1);
 	i = 0;
 	while (i < 4)
 	{
-		ndc.x = ((corners[i].x + 0.5) / img->width) * 2 - 1;
-		ndc.y = 1 - ((corners[i].y + 0.5) / img->height) * 2;
-		ndc.z = 0; // -1~1 사이면 아무 값이나 상관없음
-		corners[i] = mat4_mulmv(pv_inverse, vec4(ndc.x, ndc.y, ndc.z, 1));
-		corners[i] = vec4_mul(corners[i], corners[i].w);
-		cam->corners_world_pos[i] = (t_vec3){corners[i].x, corners[i].y, corners[i].z};
+		ndc = screen_to_ndc(corners[i][0], corners[i][1], img->width, img->height);
+		cam->corners_world_pos[i] = ndc_to_world_space(ndc, &pv_inverse);
 		i++;
 	}
 }
-
