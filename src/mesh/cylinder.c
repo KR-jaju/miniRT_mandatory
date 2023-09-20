6e0edfb3762bc4379c9e16fda275e5485c341f6f
yonaMac <yonaimyo@gmail.com>
2023-09-20 15:41:34 +0900

    Fix: 실린더 캡, 사이드 정점 별도로 구분

--- src/mesh/cylinder.c
+++ src/mesh/cylinder.c
@@ -33,20 +33,18 @@ static void	fill_vertices(t_vec3 *vertices, int sectors)
 	while (i < sectors)
 	{
 		vertices[i + 1] = point_at(d_sector * i, (float)1 / 2);
+		vertices[sectors + i + 1] = point_at(d_sector * i, (float)-1 / 2);
+		vertices[sectors * 2 + i + 1] = vertices[i + 1];
+		vertices[sectors * 3 + i + 1] = vertices[sectors + i + 1];
 		i++;
 	}
-	i = 0;
-	while (i < sectors)
-	{
-		vertices[i + sectors + 1] = point_at(d_sector * i, (float)-1 / 2);
-		i++;
-	}
-	vertices[sectors * 2 + 1] = (t_vec3){0, -0.5, 0};
+	vertices[sectors * 4 + 1] = (t_vec3){0, -0.5, 0};
 }
 
+// 윗 뚜껑은 위에서 아래로 봤을때 앞면(반시계방향), 아랫 뚜껑은 아래에서 위로 봤을때 앞면(반시계방향)
 static void	fill_indices_caps(int *indices, int sectors, int *idx)
 {
-	const int	last = sectors * 2 + 1;
+	const int	last = sectors * 4 + 1;
 	int			i;
 
 	i = 0;
@@ -55,30 +53,35 @@ static void	fill_indices_caps(int *indices, int sectors, int *idx)
 		indices[(*idx)++] = 0;
 		indices[(*idx)++] = i + 1;
 		indices[(*idx)++] = i + 2;
-		indices[(*idx)++] = i + (sectors + 1);
 		indices[(*idx)++] = i + (sectors + 2);
+		indices[(*idx)++] = i + (sectors + 1);
 		indices[(*idx)++] = last;
 		i++;
 	}
 	indices[*idx - 4] = 1;
-	indices[(*idx) - 2] = sectors + 1;
+	indices[(*idx) - 3] = sectors + 1;
 }
 
 static void	fill_indices_side(int *indices, int sectors, int *idx)
 {
-	int	i;
+	const int	offset = sectors * 2;
+	int			i;
 
 	i = 0;
 	while (i < sectors)
 	{
-		indices[(*idx)++] = i + 1;
-		indices[(*idx)++] = i + sectors + 1;
-		indices[(*idx)++] = i + sectors + 2;
-		indices[(*idx)++] = i + 2;
-		indices[(*idx)++] = i + 1;
-		indices[(*idx)++] = i + sectors + 2;
+		indices[(*idx)++] = offset + i + 1;
+		indices[(*idx)++] = offset + i + sectors + 1;
+		indices[(*idx)++] = offset + i + sectors + 2;
+		indices[(*idx)++] = offset + i + 2;
+		indices[(*idx)++] = offset + i + 1;
+		indices[(*idx)++] = offset + i + sectors + 2;
 		i++;
 	}
+	indices[*idx - 4] = offset + sectors + 1;
+	indices[*idx - 3] = offset + 1;
+	indices[*idx - 2] = offset + sectors;
+	indices[*idx - 1] = offset + sectors + 1;
 }
 
 static void	fill_indices(int *indices, int sectors)
@@ -96,15 +99,16 @@ static void	fill_vertex_normals(t_vec3 *vertex_normals, \
 	int	i;
 
 	vertex_normals[0] = (t_vec3){0, 1, 0};
+	i = 0;
 	while (i < sectors)
 	{
-		vertex_normals[i + 1] = vec3_normalize(\
-								vec3_sub(vertices[i + 1], (t_vec3){0, 1, 0}));
-		vertex_normals[sectors + i + 1] = vec3_normalize(\
-						vec3_sub(vertices[sectors + i + 1], (t_vec3){0, 1, 0}));
+		vertex_normals[i + 1] = (t_vec3){0, 1, 0};
+		vertex_normals[i + 1 + sectors] = (t_vec3){0, -1, 0};
+		vertex_normals[i + 1 + sectors * 2] = vec3_normalize((vec3_sub(vertices[i + 1], (t_vec3){0, 0.5, 0})));
+		vertex_normals[i + 1 + sectors * 3] = vec3_normalize((vec3_sub(vertices[i + 1 + sectors], (t_vec3){0, -0.5, 0})));
 		i++;
 	}
-	vertex_normals[sectors * 2 + 1] = (t_vec3){0, -1, 0};
+	vertex_normals[sectors * 4 + 1] = (t_vec3){0, -1, 0};
 }
 
 /*
@@ -113,10 +117,8 @@ vertexNormal = normalize(vertexPosition - ringCenter)
 */
 void	cylinder_init(t_mesh *mesh, int sectors)
 {
-	int	i;
-
 	mesh->n_triangles = sectors * 4;
-	mesh->n_vertices = sectors * 2 + 2;
+	mesh->n_vertices = (sectors * 4) + 2;
 	mesh->n_indices = mesh->n_triangles * 3;
 	mesh->vertices = malloc(sizeof(t_vec3) * mesh->n_vertices);
 	mesh->indices = malloc(sizeof(t_vec3) * mesh->n_indices);
