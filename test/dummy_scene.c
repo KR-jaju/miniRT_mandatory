ef6f3a1a22aeb88524ee5fdf424e72b85d2ff0c7
yonaMac <yonaimyo@gmail.com>
2023-09-20 16:28:14 +0900

    Rename: 개발용 파일 디렉토리 test/로 옮김

--- test/dummy_scene.c
+++ test/dummy_scene.c
@@ -57,30 +57,46 @@ t_object	dummy_object(t_mesh *mesh, t_vec3 color)
 int	dummy_scene(t_scene *scene)
 {
 	const int n_meshs = 2;
-	const int n_objects = 2;
+	const int n_objects = 4;
 	scene->n_meshs = n_meshs;
 	scene->n_objects = n_objects;
 	//scene->mesh = malloc(sizeof(t_object) * n_meshs);
 	scene->objects = malloc(sizeof(t_object) * n_objects);
 
 	scene->meshs[0] = mesh_plane();
-	sphere_init(&scene->meshs[1], 20, 20);
+	cylinder_init(&scene->meshs[1], 10);
 	scene->objects[0] = dummy_object(&scene->meshs[0], (t_vec3){0, 0, 1});
-	scene->objects[1] = dummy_object(&scene->meshs[1], (t_vec3){1, 0, 0});
-	scene->objects[1].position = (t_vec3){0, 1, 0};
+
+	scene->objects[1] = dummy_object(&scene->meshs[1], (t_vec3){0, 1, 0});
+	scene->objects[1].position = (t_vec3){-2, 4, 0};
+
+	scene->objects[2] = dummy_object(&scene->meshs[1], (t_vec3){0, 1, 0});
+	scene->objects[2].position = (t_vec3){0, 2, 0};
+
+	scene->objects[3] = dummy_object(&scene->meshs[1], (t_vec3){0, 1, 0});
+	scene->objects[3].position = (t_vec3){2, 1, 0};
+	// TODO: scale 행렬 확인
+
+	// sphere_init(&scene->meshs[1], 20, 20);
+	// cylinder_init(&scene->meshs[2], 10);
+	// scene->objects[0] = dummy_object(&scene->meshs[0], (t_vec3){0, 0, 1});
+	// scene->objects[1] = dummy_object(&scene->meshs[1], (t_vec3){1, 0, 0});
+	// scene->objects[1].position = (t_vec3){0, 1, 0};
+	// scene->objects[2] = dummy_object(&scene->meshs[2], (t_vec3){0, 1, 0});
+	// scene->objects[2].position = (t_vec3){0, 1, 0};
 
 
 	// printf("%d\n",scene->meshs[0].n_vertices);
 
-	scene->camera.eye = (t_vec3){0, 1, -5};
+	scene->camera.eye = (t_vec3){0, 2, -5};
 	// scene->camera.look_at = (t_vec3){0, 3, 1};
 	// scene->camera.up = (t_vec3){0, 1, 0};
 	// scene->camera.fov = 100;
 
-	scene->light.position = (t_vec3){-2, 2, 0};
+	scene->light.position = (t_vec3){-2, 4, 0};
 	scene->light.color = (t_vec3){3, 3, 3};
 	// scene->light.color = (t_vec3){1, 1, 1};
 
-	scene->ambient_light = (t_vec3){0.1, 0.1, 0.1};
+	scene->ambient_light = (t_vec3){0.3, 0.3, 0.3};
 	return (0);
 }
