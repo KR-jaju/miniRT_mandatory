7cc657659ace7d068187eeb905d5e38e61f8cb64
yonaMac <yonaimyo@gmail.com>
2023-09-21 00:05:28 +0900

    Fix: vec3_interpolate 정규화하여 반환하도록 고침

--- lib/libmath/interpolate.c
+++ lib/libmath/interpolate.c
@@ -5,11 +5,12 @@ t_vec3	vec3_interpolate(t_vec3 *vectors, float *ratio, int n_vectors)
 	t_vec3	v;
 	int		i;
 
+	v = (t_vec3){0, 0, 0};
 	i = 0;
 	while (i < n_vectors)
 	{
 		v = vec3_add(v, vec3_mul(vectors[i], ratio[i]));
 		i++;
 	}
-	return (vec3_mul(v, n_vectors));
+	return (vec3_normalize(v));
 }
