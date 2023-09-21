81195b65c6ca851b6a321316320c1b3b55d92c84
yonaMac <yonaimyo@gmail.com>
2023-09-21 19:19:03 +0900

    Feat: 변경된 파싱 에러 처리 로직 마저 반영

--- src/parser/parse_float.c
+++ src/parser/parse_float.c
@@ -1,13 +1,21 @@
 
 #include "libft.h"
+#include "parser.h"
+#include "debug.h"
 
+// 필요시 포맷 체크와 파싱 함수 나눌 것 (25줄 제한)
+static bool		_is_valid_float(const char *str_ref);
+static float	_parse_float(const char **str_ref);
+
+// DEBUG
 float	parse_float(const char **str_ref)
 {
 	float	f;
 	float	exp;
 
+	// 음수인 경우와 시작지점이 '0'인 경우 문제 발생
 	if (!('1' <= **str_ref && **str_ref <= '9'))
-		exit(1);
+		handle_parse_error(ERROR_INVALID_LINE_FORMAT);
 	f = 0;
 	while (ft_isdigit(**str_ref))
 	{
--- src/parser/parse_vec3.c
+++ src/parser/parse_vec3.c
@@ -3,16 +3,21 @@
 #include "libmath.h"
 #include <stdlib.h>
 
+// 필요시 포맷 체크와 파싱 함수 나눌 것 (25줄 제한)
+static bool		_is_valid_vec3(const char *str_ref);
+static t_vec3	_parse_vec3_(const char **str_ref);
+
+// DEBUG
 t_vec3	parse_vec3(const char **str_ref)
 {
 	t_vec3	v;
 
 	v.x = parse_float(str_ref);
 	if (*(*str_ref)++ != ',')
-		exit(1);
+		handle_parse_error(ERROR_INVALID_LINE_FORMAT);
 	v.y = parse_float(str_ref);
 	if (*(*str_ref)++ != ',')
-		exit(1);
+		handle_parse_error(ERROR_INVALID_LINE_FORMAT);
 	v.z = parse_float(str_ref);
 	return (v);
 }
