e3fa68af9d4cee341f0ce3ad5064784efd4111d6
yonaMac <yonaimyo@gmail.com>
2023-09-21 13:25:51 +0900

    Feat: mat4_inverse 임시 구현

--- lib/libmath/matrix4.c
+++ lib/libmath/matrix4.c
@@ -64,25 +64,77 @@ t_mat4		mat4_transpose(t_mat4 orig)
 	return (m);
 }
 
-// float	mat4_cofactor(t_mat4 mat, int row, int col)
-// {
-// 	int	i;
-// 	int	j;
+// TODO: 형식 맞춰 재작성
+t_mat4 mat4_inverse(t_mat4 m) {
+    t_mat4 result = mat4_identity();  // 결과 역행렬 초기화
+    const int n = 4;
+	int row[4];
+	int col[4];
+	float temp[4];
 
-// 	i = 0;
-// 	j = 0;
-// 	while (i < 4)
-// 	{
-// 		j = 0;
-// 		while (j < 4)
-// 		{
-// 			j++;
-// 		}
-// 		i++;
-// 	}
-// }
+    // 가우스-조르당 소거법을 사용하여 역행렬 계산
+    for (int i = 0; i < n; i++)
+	{
+        float max = 0.0f;
+        for (int j = 0; j < n; j++)
+		{
+            if (row[j] != 1)
+			{
+                for (int k = 0; k < n; k++)
+				{
+                    if (col[k] == 0)
+					{
+                        float abs_value = fabs(m.e[j][k]);
+                        if (abs_value >= max)
+						{
+                            max = abs_value;
+                            row[j] = 1;
+                            col[k] = 1;
+                        }
+                    }
+                }
+            }
+        }
 
-// t_mat4	mat4_inverse(t_mat4 mat)
-// {
+        if (row[i] != 1 || col[i] != 1) {
+            // 역행렬을 찾을 수 없는 경우 예외 처리
+            return (mat4_identity());
+        }
 
-// }
+        for (int j = 0; j < n; j++)
+		{
+            if (j != i)
+			{
+                temp[j] = m.e[j][i] / m.e[i][i];
+                for (int k = 0; k < n; k++)
+                    if (k != i)
+                        m.e[j][k] -= temp[j] * m.e[i][k];
+            }
+        }
+
+        for (int j = 0; j < n; j++) {
+            if (j != i) {
+                m.e[j][i] = -temp[j] / m.e[i][i];
+            }
+        }
+
+        for (int j = 0; j < n; j++)
+		{
+            if (j != i)
+                for (int k = 0; k < n; k++)
+                    if (k != i)
+                        m.e[j][k] += m.e[j][i] * m.e[i][k];
+        }
+
+        m.e[i][i] = -1.0f / m.e[i][i];
+        for (int j = 0; j < n; j++)
+            if (j != i)
+                m.e[j][i] *= m.e[i][i];
+    }
+
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            result.e[i][j] = m.e[i][j];
+
+    return result;
+}
