0d54f6d457dfe43eab952ee1a23bcb86719760b9
yonaMac <yonaimyo@gmail.com>
2023-09-21 16:08:15 +0900

    Test: 더미 씬 파일에 카메라 테스트 코드 추가

--- test/dummy_scene.c
+++ test/dummy_scene.c
@@ -3,6 +3,17 @@
 #include "mesh.h"
 #include "debug.h"
 
+enum e_meshs
+{
+	PLANE,
+	SPHERE,
+	CYLINDER
+};
+
+t_vec3 red = (t_vec3){1, 0, 0};
+t_vec3 green = (t_vec3){0, 1, 0};
+t_vec3 blue = (t_vec3){0, 0, 1};
+
 t_object	dummy_object(t_mesh *mesh, t_vec3 color)
 {
 	t_object object;
@@ -20,19 +31,11 @@ t_object	dummy_object(t_mesh *mesh, t_vec3 color)
 	return (object);
 }
 
-
-enum e_meshs
-{
-	PLANE,
-	SPHERE,
-	CYLINDER
-};
-
 void set_objects(t_scene *scene)
 {
-	t_vec3 red = (t_vec3){1, 0, 0};
-	t_vec3 green = (t_vec3){0, 1, 0};
-	t_vec3 blue = (t_vec3){0, 0, 1};
+	const int n_objects = 4;
+	scene->n_objects = n_objects;
+	scene->objects = malloc(sizeof(t_object) * n_objects);
 
 	scene->objects[0] = dummy_object(&scene->meshs[PLANE], blue);
 
@@ -44,22 +47,24 @@ void set_objects(t_scene *scene)
 
 	scene->objects[3] = dummy_object(&scene->meshs[CYLINDER], green);
 	scene->objects[3].position = (t_vec3){2, 1, 0};
+}
 
-	// sphere_init(&scene->meshs[1], 20, 20);
-	// cylinder_init(&scene->meshs[2], 10);
-	// scene->objects[0] = dummy_object(&scene->meshs[0], (t_vec3){0, 0, 1});
-	// scene->objects[1] = dummy_object(&scene->meshs[1], (t_vec3){1, 0, 0});
-	// scene->objects[1].position = (t_vec3){0, 1, 0};
-	// scene->objects[2] = dummy_object(&scene->meshs[2], (t_vec3){0, 1, 0});
-	// scene->objects[2].position = (t_vec3){0, 1, 0};
+void set_objects_test_camera(t_scene *scene)
+{
+	const int n_objects = 1;
+	scene->n_objects = n_objects;
+	scene->objects = malloc(sizeof(t_object) * n_objects);
+
+	scene->objects[0] = dummy_object(&scene->meshs[SPHERE], blue);
+	scene->objects[0].position = (t_vec3){0, 0, 5};
 }
 
 void set_camera(t_scene *scene)
 {
-	scene->camera.position = (t_vec3){0, 2, -5};
+	scene->camera.position = (t_vec3){0, 0, 0};
 	scene->camera.right = vec3_normalize((t_vec3){1, 0, 0});
 	scene->camera.up = vec3_normalize((t_vec3){0, 1, 0});
-	scene->camera.forward = vec3_normalize((t_vec3){0, -2, 5});
+	scene->camera.forward = vec3_normalize((t_vec3){0, 0, 1});
 	scene->camera.fov = 100;
 }
 
@@ -70,21 +75,18 @@ void set_light(t_scene *scene)
 	scene->ambient_light = (t_vec3){0.3, 0.3, 0.3};
 }
 
-
 int	dummy_scene(t_scene *scene)
 {
 	// 초기화 작업
 	const int n_meshs = 3;
-	const int n_objects = 4;
 	scene->n_meshs = n_meshs;
-	scene->n_objects = n_objects;
-	scene->objects = malloc(sizeof(t_object) * n_objects);
 	plane_init(&scene->meshs[0]);
 	sphere_init(&scene->meshs[1], 10, 10);
 	cylinder_init(&scene->meshs[2], 10);
 
 	// 오브젝트, 카메라, 빛 세팅
-	set_objects(scene);
+	// set_objects(scene);
+	set_objects_test_camera(scene);
 	set_camera(scene);
 	set_light(scene);
 	return (0);
