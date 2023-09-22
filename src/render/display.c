8092ba013eabf40d8bf7322429a781d08ba4804d
yonaMac <yonaimyo@gmail.com>
2023-09-23 04:38:53 +0900

    Refactor: render_display 함수 로직 변경

--- src/render/display.c
+++ src/render/display.c
@@ -1,16 +1,13 @@
+#include <stdio.h>
+#include "miniRT.h"
 #include "mlx_api.h"
 #include "render.h"
 #include "console.h"
-#include "miniRT.h"
+#include "settings.h"
 
 t_vec3	render_pixel(int x, int y, t_scene *scene, t_image *img);
 
-/*
-1. 현재 처리해야하는 픽셀 컬러 계산
-2. 계산된 픽셀 이미지에 반영
-3. 이미지를 윈도우에 put
-*/
-void	render_image(int progress, t_scene *scene, t_image *img, bool *done)
+void	render_image(int progress, t_scene *scene, t_image *img)
 {
 	t_pixel	p;
 
@@ -18,30 +15,36 @@ void	render_image(int progress, t_scene *scene, t_image *img, bool *done)
 	p.x = progress - (p.y * img->width);
 	p.color = render_pixel(p.x, p.y, scene, img);
 	put_pixel_to_image(p, img);
-	if (progress == img->n_pixels)
-		*done = true;
 }
 
-// TODO: 현재는 window에 put하는 작업과 콘솔 출력을 이 함수에서 전부 하고 있는데
-// 차후 render_loop 함수 별도로 만들어 render_display 함수 호출 + 콘솔 출력 
+/*
+1. 현재 처리해야하는 픽셀 컬러 계산
+2. 계산된 픽셀 이미지에 반영
+3. 이미지를 윈도우에 put
+*/
 int	render_display(t_program_data *data)
 {
-	static bool	done = false;
-	static int	progress = 0;
+	static const int	interval = (IMAGE_WIDTH * IMAGE_HEIGHT) \
+									* IMAGE_PUT_INTERVAL_PERCENT / 100;
+	static int			progress = 0;
+	static bool			finished = false;
 
-	if (done)
+	if (finished)
 		return (0);
-	if (progress == 0)
-		console_msg(MSG_RENDER_START"\n"MSG_RENDER_PROGRESS);
-	render_image(progress, data->scene, data->img, &done);
-	if (progress % 5000 == 0 || done)
+	render_image(progress, data->scene, data->img);
+	progress++;
+	if (progress % interval == 0 || progress == data->img->n_pixels)
 	{
 		mlx_put_image_to_window(data->mlx->conn, \
 								data->mlx->win, data->img->addr, 0, 0);
-		printf("progress: %d / %d\n", progress, data->img->n_pixels);
+		printf(ANSI_CYAN"progress: %d / %d (%.2f%%)\n"ANSI_RESET, \
+								progress, data->img->n_pixels, \
+								(float)progress / data->img->n_pixels * 100);
 	}
-	progress++;
-	if (done)
-		console_msg(MSG_RENDER_DONE);
+	if (progress == data->img->n_pixels)
+	{
+		finished = true;
+		console_msg(ANSI_GREEN""MSG_RENDER_FINISHED);
+	}
 	return (0);
 }
